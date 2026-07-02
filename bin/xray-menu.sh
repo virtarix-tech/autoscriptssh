ff049685d74778bbd8453c2591adcdc17caf8f52
virtarix <kishkith6@gmail.com>
2026-07-03 01:11:40 +0300

    Add files via upload

---
+++ bin/xray-menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Xray Menu
+clear
+echo "v2ray menu service"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "[ 1 ] create vmess acount"
+# ... [add others 2-10]
+read -p "Select an option [1-10]: " opt
+# Add case statement here to call functions from xray_utils.sh
