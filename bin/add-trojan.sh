449cec5171154bcbdafc583e6fdcec013f547842
virtarix <kishkith6@gmail.com>
2026-07-03 02:37:49 +0300

    Modify add-trojan.sh to include password for user
    Updated jq command to add user with password to Xray config.

--- bin/add-trojan.sh
+++ bin/add-trojan.sh
@@ -23,7 +23,9 @@ echo -e "━━━━━━━━━━━━━━━━━━━━━━━�
 echo -e "link trojan ws   : $link"
 # Add to /opt/virtarixtech/bin/add-vmess (and similarly for vless/trojan)
 # We use jq to safely insert the user into the JSON array
-jq --arg uuid "$uuid" --arg user "$user" '.inbounds[0].settings.clients += [{"id": $uuid, "alterId": 0, "email": $user}]' /etc/xray/config.json > /etc/xray/config.json.tmp && mv /etc/xray/config.json.tmp /etc/xray/config.json
+jq --arg pass "$key" --arg user "$user" \
+'.inbounds[X].settings.clients |= map(select(.email != $user)) + [{"password": $pass, "email": $user}]' \
+/etc/xray/config.json > /etc/xray/config.json.tmp && mv /etc/xray/config.json.tmp /etc/xray/config.json
 
 # Restart Xray to apply changes
 systemctl restart xray
